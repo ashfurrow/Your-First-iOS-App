51a39df29e2180e87a402e7a44a568b4861c2d48
Ash Furrow <ash@ashfurrow.com>
2013-03-23 17:43:53 -0400

    Created groups.

---
+++ Coffee Timer/Coffee Timer/CTRTimerDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  CTRTimerDetailViewController.h
+//  Coffee Timer
+//
+//  Created by Ash Furrow on 2013-03-23.
+//  Copyright (c) 2013 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTRTimerDetailViewController : UIViewController
+
+@end
